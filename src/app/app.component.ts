27c9aefdf52e3c5339899d54d450cf7e25bf36f2
Ramesh Kumar M K <mkrk.ramesh006@gmail.com>
2023-11-06 22:44:34 +0530

    Added ngOnInit,@output EventEmitter,@viewchild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SongListComponent } from './song-list/song-list.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Angular-Assignment-2';
+
+  listCount:any;
+  isDisplay:any;
+  
+  assignboolean($event:any){
+    this.isDisplay=$event;
+    console.log('assigning new boolean :', this.isDisplay);
+  }
+
+  assignnumber($event:any){
+    this.listCount=$event;
+    console.log('assigning new count :', this.listCount);
+  }
+
+  @ViewChild(SongListComponent) songListComponent: SongListComponent = new SongListComponent();
+
+  getcount() {
+    this.songListComponent.getcount(); // Call the child component's getcount method
+  }
+
+  // ngAfterViewInit() {
+  //   this.songList = this.child.songList;
+  //   this.listCount = this.child.listCount;
+  //   this.isDisplay = this.child.isDisplay;
+  // }
+  
 }
--- src/app/song-list/song-list.component.ts
+++ src/app/song-list/song-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-song-list',
@@ -6,20 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./song-list.component.css']
 })
 export class SongListComponent {
+
+  @Output() displayboolean = new EventEmitter<boolean>;
+  @Output() countnumber = new EventEmitter<number>;
+
   newSong:string='';
   songList: string[] = ['song1','song2','song3','Song4'];
   listCount:number = this.songList.length;
   isDisplay:boolean = false;
 
+  ngOnInit(){
+    this.displayboolean.emit(this.isDisplay);
+    this.countnumber.emit(this.listCount);
+  }
+
   updateList(){
     this.songList.push(this.newSong);
     this.isDisplay=false;
+    this.displayboolean.emit(this.isDisplay);
+    this.countnumber.emit(this.songList.length);
     console.log('Updating new song :', this.newSong);
   }
 
   getcount(){
     this.isDisplay=true;
     this.listCount=this.songList.length;
+    this.displayboolean.emit(this.isDisplay);    
+    this.countnumber.emit(this.listCount);
     console.log('listCount is now:', this.listCount);
   }
+
 }
